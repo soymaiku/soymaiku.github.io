fe273f518ca75f65be448bfeea0f08ba817c667a
Mike Abrasaldo <mikeandrew2199@gmail.com>
2025-09-01 14:32:24 +0800

    v.1.0

--- js/splash.js
+++
@@ -1,8 +0,0 @@
-// var splash = document.getElementById("splash");
-
-// function showSplash(){
-//     console.log("hi");
-//     setTimeout(()=>{
-//         splash.classList.add('display-none');
-//     }, 1000);
-// }
