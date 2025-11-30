c17dc46ff83a6640bf0847fc7da27f7d80a88ec4
Mike Abrasaldo <mikeandrew2199@gmail.com>
2025-11-30 19:27:53 +0800

    Revert "v.1.0"
    This reverts commit fe273f518ca75f65be448bfeea0f08ba817c667a.

---
+++ js/splash.js
@@ -0,0 +1,8 @@
+// var splash = document.getElementById("splash");
+
+// function showSplash(){
+//     console.log("hi");
+//     setTimeout(()=>{
+//         splash.classList.add('display-none');
+//     }, 1000);
+// }
